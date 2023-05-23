1475aa51d53290a65f0116849898cd4877e9e5ec
jgarcia-ASD <jgarcia@grupoasd.com>
2023-05-23 13:40:03 -0500

    Termine el modulo conceptos basicos

---
+++ Sololearn/Introducción a Java/Conceptos Basicos/Multiples_declaraciones.java
@@ -0,0 +1,29 @@
+public class Multiples_declaraciones {
+    public static void main(String[] args) {
+        //podemos tener varias salidas de codigo
+        System.out.println("Hola");
+        System.out.println("Mundo");
+
+        System.out.println("-------------------------");
+        //si no ponemos ln nos deja todo en la misma linea
+        System.out.print("Hola");
+        System.out.print(" Mundo pegado");
+
+        System.out.println(" ");
+        System.out.println("-------------------------");
+        //podemos tambien poner numeros
+        System.out.println("Puntos:");
+        System.out.println(500);
+
+
+        //este codigo tenia un error y lo corregi
+        /*
+        public class Program {
+            public static void main(String[] args) {
+                System.out.println("My firs Java Code Coach!");
+            }
+        }
+        
+        */ 
+    }
+}
